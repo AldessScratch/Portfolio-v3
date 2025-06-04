bfb529dcd249c12c947f560d4bc5f42ba2904629
Aldess <106018979+AldessScratch@users.noreply.github.com>
2025-06-04 11:11:02 +0200

    Create page.tsx

---
+++ src/app/admin-dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function AdminDashboard() {
+    return (
+        <div className="flex items-center justify-center w-full h-screen">
+            <div className="flex fixed left-0 items-center justify-center">
+                <h2>Dashboard</h2>
+                <ul className="flex flex-col items-start">
+                    <li className="mb-2"></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
