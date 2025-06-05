0ab8fb90bbef0283fce6078f505861c117976d79
Aldess <106018979+AldessScratch@users.noreply.github.com>
2025-06-05 20:48:23 +0200

    Update page.tsx

--- src/app/admin-dashboard/page.tsx
+++ src/app/admin-dashboard/page.tsx
@@ -1,12 +1,15 @@
+import ThemeToggle from "@/components/themetoggle"
+
 export default function AdminDashboard() {
     return (
-        <div className="flex items-center justify-center w-full h-screen">
+        <div className="flex items-center justify-center w-full h-screen bg-gradient-to-br from-slate-100 to-slate-200 dark:from-slate-900 dark:to-slate-800 ease-in-out">
             <div className="flex fixed left-0 items-center justify-center w-30">
                 <h2>Dashboard</h2>
                 <ul className="flex flex-col items-start">
                     <li className="mb-2">Home</li>
                     <li className="mb-2">Analytics</li>
                 </ul>
+                <ThemeToggle />
             </div>
         </div>
     )
