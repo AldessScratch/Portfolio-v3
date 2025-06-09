0b45a814065e0ef73cff320b04122114983fb20e
Aldess <106018979+AldessScratch@users.noreply.github.com>
2025-06-09 18:24:34 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,13 @@
-export { default } from "next-auth/middleware"
+import { NextRequest, NextResponse } from "next/server";
 
+export { default } from "next-auth/middleware"
+// src/middleware.ts
+export function middleware(request: NextRequest) {
+  request.headers.set("X-Frame-Options", "DENY");
+  request.headers.set("X-Content-Type-Options", "nosniff");
+  request.headers.set("X-XSS-Protection", "1; mode=block");
+  return NextResponse.next();
+}
 export const config = { 
     matcher: [
         "/dashboard"
